a9a1e8fbfb4ff87f2240f2d949df28a57f7796fa
Jeff Terstriep <jefft@illinois.edu>
2017-08-17 06:38:29 -0500

    Add install_requires to catch dependencies.

--- setup.py
+++ setup.py
@@ -1,12 +1,21 @@
 from setuptools import setup
 
-setup(name='pyclowder',
-      version='2.0.0',
-      packages=['pyclowder'],
-      entry_points={
-          'console_scripts': [
-              'clowder = pyclowder.__main__:main',
-          ]
-      },
-      zip_safe=True,
-      )
+setup(
+    name='pyclowder',
+    version='2.0.0',
+    packages=['pyclowder'],
+    zip_safe=True,
+
+    install_requires=[
+        'enum34',
+        'pika',
+        'PyYAML',
+        'requests',
+    ],
+
+    entry_points={
+        'console_scripts': [
+            'clowder = pyclowder.__main__:main',
+        ]
+    },
+)
